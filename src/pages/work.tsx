675b84b6740e752a7f26ec4c60bedaa5fc92f0c5
wantfee <59550481@qq.com>
2023-02-25 13:02:36 +0800

    修改完善作品页面

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import Layout from "@theme/Layout";
+import React from 'react';
+import Layout from '@theme/Layout';
 
-const work = () => {
+export default function Hello() {
   return (
-    <Layout>
-      <main>
-        <div>work</div>
-      </main>
+    <Layout title="Work" description="Here's the work page">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          All my works will display here.
+        </p>
+      </div>
     </Layout>
   );
-};
-
-export default work;
+}
