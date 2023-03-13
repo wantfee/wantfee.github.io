9ee88c826bc11bae351ca400c82ceb3f732ca5ac
wantfee <59550481@qq.com>
2023-03-13 16:53:33 +0800

    1.更新两篇文章;2,设计更新作品<多少>缩略图

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Layout from "@theme/Layout";
 import classes from "./work.module.css";
+import moreOrLess from '@site/static/img/more-or-less.png';
 
 export default function Works() {
   return (
     <Layout title="Work" description="Here's the work page">
       <div className={classes.allWorkContainer}>
-        <div className={classes.work}></div>
-        <div className={classes.work}></div>
-        <div className={classes.work}></div>
-        <div className={classes.work}></div>
-        <div className={classes.work}></div>
-        <div className={classes.work}></div>
+        <div className={classes.work}>
+        <img src={moreOrLess} style={{width:"100%", marginBottom:"30px"}}/>
+        <h3 className={classes.workTitle}>多少</h3>
+        </div>
+        <div className={classes.workEmpty}>即将上线...</div>
+        <div className={classes.workEmpty}>即将上线...</div>
+        {/* <div className={classes.workEmpty}>即将上线...</div>
+        <div className={classes.workEmpty}>即将上线...</div>
+        <div className={classes.workEmpty}>即将上线...</div> */}
       </div>
     </Layout>
   );
